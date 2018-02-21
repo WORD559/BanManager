911e699f27f4c85d2e4de2882754cb4e6a3b83a9
WORD559 <josh.barrass.work@gmail.com>
2018-02-21 11:38:16 +0000

    Added an AJAX loader gif and two functions for replacing the submit button of a form with the gif (this prevents double submits). In addition, I've added a function for encoding an object into form data. This also escapes special characters, which is essential for instances such as passwords with special characters.

--- client/makeasync.js
+++ client/makeasync.js
@@ -10,3 +10,38 @@ function MakeAsyncRequest(method,url,callback,data=null,content_type="applicatio
     xhttp.setRequestHeader("Content-type",content_type);
     xhttp.send(data);
 }
+
+function encodeFormData(d) {
+    var set = false;
+    var encoded = "";
+    for (var key in d) {
+        if (set) {
+            encoded += "&";
+        } else {
+            set = true;
+        }
+        encoded += encodeURIComponent(key)+"="+encodeURIComponent(d[key]);
+    }
+    return encoded;
+}        
+
+function SetAjaxLoader(button,image="/ajax-loader.gif") {
+    var form = button.parentNode
+    form.removeChild(button);
+    var loader = document.createElement("img");
+    loader.src = image;
+    loader.id = "ajax-loader";
+    form.appendChild(loader);
+}
+
+function UnsetAjaxLoader(button_id,button_text,f) {
+    var button = document.createElement("input");
+    button.id = button_id;
+    button.type = "submit";
+    button.value = button_text;
+    var loader = document.getElementById("ajax-loader");
+    var form = loader.parentNode
+    form.removeChild(loader);
+    form.appendChild(button);
+    document.getElementById(button_id).onclick = function() {f(); return false;};/*For some reason I have to stick this after otherwise it just doesn't work.*/
+}
