729ba75a4173d7ac6da96e7902ac8ecf53cc6c8f
WORD559 <josh.barrass.work@gmail.com>
2018-02-23 15:21:40 +0000

    Added link back to the dashboard on the menu bar

--- client/menubar.js
+++ client/menubar.js
@@ -1,6 +1,7 @@
 document.write('<table class="account-widget">\
 <tr>\
 <td style="border-right:solid 1px black">Welcome, <span class="highlighted-var username" id="username">username</span>!<br><a href="/logout">Logout</a></td>\
+<td><a href="/dash">Dashboard</a></td>\
 <td><a href="/incidents">Incidents (<span class="highlighted-var" id="incidents">0</span> Unresolved)</a></td>\
 </tr>\
 </table>');
